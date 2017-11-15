30e4bf110dfbe45f3a3df37d8083a7d6cc3bdf2a
Johan Froberg <johfr198@student.liu.se>
2017-11-15 13:34:40 +0100

    Kalender

--- App.js
+++ App.js
@@ -1,32 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import moment from 'moment'; // 2.18.1
+import CalendarStrip from 'react-native-calendar-strip'; // 1.2.1
 
-export default class App extends React.Component {
+let datesWhitelist = [
+  {
+    start: moment().subtract(1000, 'days'),
+    end: moment().add(1000, 'days'),// total 4 days enabled
+  },
+];
+
+export default class Example extends Component {
   render() {
-      
-      let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
-      
     return (
-      <View style={styles.container}>
-
-        <Text>Open up App.js to start writing loads of shit about Jonsa!</Text>
-        <Text>Open up App.js to start working on your app!</Text>
-          <Text>Hejhej.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-
+      <View>
+        <CalendarStrip
+          calendarAnimation={{ type: 'sequence', duration: 30 }}
+          daySelectionAnimation={{
+            type: 'border',
+            duration: 200,
+            borderWidth: 1,
+            borderHighlightColor: 'white',
+          }}
+          style={{ height: 100, paddingTop: 20, paddingBottom: 10 }}
+          calendarHeaderStyle={{ color: 'white' }}
+          calendarColor={'##4682B4'}
+          dateNumberStyle={{ color: 'white' }}
+          dateNameStyle={{ color: 'white' }}
+          highlightDateNumberStyle={{ color: 'yellow' }}
+          highlightDateNameStyle={{ color: 'yellow' }}
+          disabledDateNameStyle={{ color: 'grey' }}
+          disabledDateNumberStyle={{ color: 'grey' }}
+          datesWhitelist={datesWhitelist}
+          iconContainer={{ flex: 0.1 }}
+        />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
